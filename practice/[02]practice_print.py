a533c0e70ddb1906c4186c62f905e7ca5815feca
gongy044 <gongy049@126.com>
2016-07-15 16:32:29 +0800

    Delete [02]practice_print.py

--- practice/[02]practice_print.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# practice how to use input() function, input() function always returns a string
-
-a = input('what are you thinking about? >>>  ')
-print (a)
-b = input('please enter an integer >>>  ')
-print (b)
-print ('input method always return a string')
-print (a + b)
